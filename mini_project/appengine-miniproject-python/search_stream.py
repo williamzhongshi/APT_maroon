c73603a39b476b84068fa0f371e4777d83677a5d
williamzhongshi <williamzhongshi@yahoo.com>
2017-10-05 20:29:29 -0500

    use search api to search, only display 5 result if more than that.

--- mini_project/appengine-miniproject-python/search_stream.py
+++ mini_project/appengine-miniproject-python/search_stream.py
@@ -23,6 +23,7 @@ import cloudstorage as gcs
 from google.appengine.api import app_identity, mail, users, search
 from google.appengine.ext import ndb
 from entities_def import User, Photo, Stream
+
 import jinja2
 import webapp2
 
@@ -75,60 +76,63 @@ class SearchStreams(webapp2.RequestHandler):
         #target_query
 
         # IF USE GOOGLE SEARCH API
-        # index = search.Index(name="streamsearch")
-        # target_query = Stream.query()
-        # targets = target_query.fetch()
-        # for target in targets:
-        #     fields = [
-        #         search.TextField(name="name", value=target.name)
-        #     ]
-        #     for tag in target.tags:
-        #         fields.append(search.TextField(name="tags", value=tag))
-        #     d = search.Document(fields=fields, language='en')
-        #     search.Index(name="streamsearch").put(d)
-        #
-        # index = search.Index("streamsearch")
-        #
-        # search_results = index.search("%s in name OR %s in tags" % (search_text, search_text))
-        #
-        # #logging.info("%s", dir(search_results))
-        # logging.info("Found %d number of results" % len(search_results.results))
-        #
-        # for i in search_results:
-        #     logging.info("Found %s", i)
-        #     logging.info("Found %s", dir(i))
-        #
-        # final_targets = []
-
-
-        #
-        #
-        # user_obj = User()
-        # user_obj.username =users.get_current_user()
-        # user_obj.email = users.get_current_user().email()
-
-        # stream = Stream(parent=user_key('abc'))
-        # stream.name = stream_name
-        # stream.cover_image = pic_url
-        # stream.tags = [tags]
-        # stream.subs = [subscriber_list]
-
-        #stream.put()
-
-        # Just try to retrieve from NDB
-
-        # WORKS BUT NOT USING SEARCH API
+        index = search.Index(name="search_stream")
         target_query = Stream.query()
         targets = target_query.fetch()
-        final_targets = []
-
         for target in targets:
-            logging.info("target name %s, tags: %s, search text %s " % (target.name, target.tags, search_text))
-            if search_text in target.name or search_text in str(target.tags):
-                final_targets.append(target)
+            fields = [
+                search.TextField(name="name", value=target.name)
+            ]
+            for tag in target.tags:
+                fields.append(search.TextField(name="tags", value=tag))
+            d = search.Document(fields=fields, language='en')
+            search.Index(name="search_stream").put(d)
 
+        #index = search.Index()
+
+        #search_results = index.search("%s in name OR %s in tags" % (search_text, search_text))
+        search_results = index.search(search_text)
+        #logging.info("%s", dir(search_results))
+        logging.info("Found %d number of results" % len(search_results.results))
+
+        final_targets = []
+        counter = 0
+        for i in search_results:
+            found_name = i.field("name".encode('utf-8')).value.decode('utf-8')
+            logging.info("Found %s" % found_name)
+            if counter < 5:
+                target_query = Stream.query(Stream.name == found_name)
+                target = target_query.fetch()[0]
+                final_targets.append(target)
+                counter += 1
+            else:
+                break
+        logging.info("Listing %d of streams" % len(final_targets))
         for i in final_targets:
             logging.info("found %s", i.name)
+        # remove all indexes
+        while True:
+            # until no more documents, get a list of documents,
+            # constraining the returned objects to contain only the doc ids,
+            # extract the doc ids, and delete the docs.
+            document_ids = [document.doc_id for document in index.get_range(ids_only=True)]
+            if not document_ids:
+                break
+            index.delete(document_ids)
+
+        # Just try to retrieve from NDB
+        # # WORKS BUT NOT USING SEARCH API
+        # target_query = Stream.query()
+        # targets = target_query.fetch()
+        # final_targets = []
+        #
+        # for target in targets:
+        #     logging.info("target name %s, tags: %s, search text %s " % (target.name, target.tags, search_text))
+        #     if search_text in target.name or search_text in str(target.tags):
+        #         final_targets.append(target)
+        #
+        # for i in final_targets:
+        #     logging.info("found %s", i.name)
 
         template_values = {
             'stream': final_targets,
@@ -136,7 +140,7 @@ class SearchStreams(webapp2.RequestHandler):
 
         template = JINJA_ENVIRONMENT.get_template('SearchStream.html')
         self.response.write(template.render(template_values))
-        
+
 
 # [START app]
 app = webapp2.WSGIApplication([
